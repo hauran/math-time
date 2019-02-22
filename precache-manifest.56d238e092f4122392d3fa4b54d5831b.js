e07625fe52300f7accfd117d4d459c8bb81d10a3
rmai <richardmai@gmail.com>
2019-02-21 20:25:59 -0500

    Updates

--- precache-manifest.56d238e092f4122392d3fa4b54d5831b.js
+++ precache-manifest.56d238e092f4122392d3fa4b54d5831b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/math-time/static/js/runtime~main.e6001782.js"
   },
   {
-    "revision": "e267ed4d5cb6925037d7",
-    "url": "/math-time/static/js/main.e267ed4d.chunk.js"
+    "revision": "70c64579dfe0d0cc709f",
+    "url": "/math-time/static/js/main.70c64579.chunk.js"
   },
   {
     "revision": "7aea24783048de82a2a0",
     "url": "/math-time/static/js/2.7aea2478.chunk.js"
   },
   {
-    "revision": "e267ed4d5cb6925037d7",
+    "revision": "70c64579dfe0d0cc709f",
     "url": "/math-time/static/css/main.b6429f98.chunk.css"
   },
   {
-    "revision": "a33d06fb59fbc438dc904591808c6ed2",
+    "revision": "e3d0f59e972f8c60d432b745e01a8d75",
     "url": "/math-time/index.html"
   }
 ];
