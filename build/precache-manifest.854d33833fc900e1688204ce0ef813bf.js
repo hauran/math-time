633abfa889a077ab85994f2ce13976196264f30d
rmai <richardmai@gmail.com>
2019-02-23 01:11:25 -0500

    build

---
+++ build/precache-manifest.854d33833fc900e1688204ce0ef813bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6c339172bb535dff91e0",
+    "url": "/static/js/main.6c339172.chunk.js"
+  },
+  {
+    "revision": "01a41d809c97b10a59dc",
+    "url": "/static/js/2.01a41d80.chunk.js"
+  },
+  {
+    "revision": "6c339172bb535dff91e0",
+    "url": "/static/css/main.887a0d05.chunk.css"
+  },
+  {
+    "revision": "5154760c6a7766d9900c25b8485e1a17",
+    "url": "/index.html"
+  }
+];
