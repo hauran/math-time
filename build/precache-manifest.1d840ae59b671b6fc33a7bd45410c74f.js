6b4c690dc3890381f65a53e74e05a486b0fcef2d
Richard Mai <richardmai@gmail.com>
2019-12-10 01:39:37 -0500

    build

---
+++ build/precache-manifest.1d840ae59b671b6fc33a7bd45410c74f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82a5536c86f242bbe5ca88e4ed4bc77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4dbfa4c3ac25ddf9cb1",
+    "url": "/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "8c7b65dda4ed7ad5f5b2",
+    "url": "/static/css/main.1a623c1b.chunk.css"
+  },
+  {
+    "revision": "f4dbfa4c3ac25ddf9cb1",
+    "url": "/static/js/2.7b083b47.chunk.js"
+  },
+  {
+    "revision": "c76a8c75c1f9e4220a3bd81741d9248d",
+    "url": "/static/js/2.7b083b47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c7b65dda4ed7ad5f5b2",
+    "url": "/static/js/main.0106a6d1.chunk.js"
+  },
+  {
+    "revision": "e155333093769ae82d77",
+    "url": "/static/js/runtime-main.4ff7bd8b.js"
+  }
+]);
